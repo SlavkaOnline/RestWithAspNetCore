933471f65da3b130de41146720d2762ab8dc0012
rafaelldi <rival.abdrakhmanov@outlook.com>
2019-11-23 15:39:17 +0300

    Add controller

---
+++ Models/Article.cs
@@ -0,0 +1,9 @@
+namespace RestWithAspNetCore.Models
+{
+    public class Article
+    {
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+    }
+}
