69ff10e2641507694e41d7b7f407f984993e9d68
rafaelldi <rival.abdrakhmanov@outlook.com>
2019-11-23 18:02:02 +0300

    Change solution

--- Models/Article.cs
+++
@@ -1,9 +0,0 @@
-namespace RestWithAspNetCore.Models
-{
-    public class Article
-    {
-        public string Description { get; set; }
-        public string Author { get; set; }
-        public string Content { get; set; }
-    }
-}
--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,23 +1,32 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace RestWithAspNetCore
+namespace Server
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
         }
-
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
