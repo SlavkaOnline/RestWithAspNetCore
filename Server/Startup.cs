dec63f5edae80eed7fd799ea8a54fd206ccfb160
rafaelldi <rival.abdrakhmanov@outlook.com>
2019-11-24 21:56:50 +0300

    Add openapi

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -10,12 +10,15 @@ namespace Server
         {
             services.AddControllers();
             services.AddSingleton<ArticleService>();
+            services.AddOpenApiDocument();
         }
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
             app.UseRouting();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
